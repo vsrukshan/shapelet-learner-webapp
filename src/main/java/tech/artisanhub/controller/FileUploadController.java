4c1f735e0e8164f9f41eadbf31b8f48bb4dd75cf
rnavagamuwa <randikanavagamuwa@gmail.com>
2017-01-08 17:41:24 +0530

    New UplaodFile class is used

--- src/main/java/tech/artisanhub/controller/FileUploadController.java
+++ src/main/java/tech/artisanhub/controller/FileUploadController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import tech.artisanhub.fileHandler.UploadFile;
 
 /**
  * Handles requests for the application file upload requests
@@ -30,35 +31,7 @@ public class FileUploadController {
     @ResponseBody
     String uploadFileHandler(@RequestParam("file") MultipartFile file) {
 
-        if (!file.isEmpty()) {
-            try {
-                byte[] bytes = file.getBytes();
-
-                // Creating the directory to store file
-                String rootPath = System.getProperty("catalina.home");
-                File dir = new File(rootPath + File.separator + "uploads");
-                if (!dir.exists())
-                    dir.mkdirs();
-
-                // Create the file on server
-                File serverFile = new File(dir.getAbsolutePath()
-                        + File.separator + file.getOriginalFilename());
-                BufferedOutputStream stream = new BufferedOutputStream(
-                        new FileOutputStream(serverFile));
-                stream.write(bytes);
-                stream.close();
-
-                logger.info("Server File Location="
-                        + serverFile.getAbsolutePath());
-
-                return "You successfully uploaded file=" + file.getOriginalFilename();
-            } catch (Exception e) {
-                return "You failed to upload " + file.getOriginalFilename() + " => " + e.getMessage();
-            }
-        } else {
-            return "You failed to upload " + file.getOriginalFilename()
-                    + " because the file was empty.";
-        }
+        return UploadFile.uploadSingleFile(file);
     }
 
     /**
@@ -67,42 +40,8 @@ public class FileUploadController {
     @RequestMapping(value = "/uploadMultipleFile", method = RequestMethod.POST)
     public
     @ResponseBody
-    String uploadMultipleFileHandler(@RequestParam("name") String[] names,
-                                     @RequestParam("file") MultipartFile[] files) {
+    String uploadMultipleFileHandler(@RequestParam("file") MultipartFile[] files) {
 
-        if (files.length != names.length)
-            return "Mandatory information missing";
-
-        String message = "";
-        for (int i = 0; i < files.length; i++) {
-            MultipartFile file = files[i];
-            String name = names[i];
-            try {
-                byte[] bytes = file.getBytes();
-
-                // Creating the directory to store file
-                String rootPath = System.getProperty("catalina.home");
-                File dir = new File(rootPath + File.separator + "uploads");
-                if (!dir.exists())
-                    dir.mkdirs();
-
-                // Create the file on server
-                File serverFile = new File(dir.getAbsolutePath()
-                        + File.separator + name);
-                BufferedOutputStream stream = new BufferedOutputStream(
-                        new FileOutputStream(serverFile));
-                stream.write(bytes);
-                stream.close();
-
-                logger.info("Server File Location="
-                        + serverFile.getAbsolutePath());
-
-                message = message + "You successfully uploaded file=" + name
-                        + " ";
-            } catch (Exception e) {
-                return "You failed to upload " + name + " => " + e.getMessage();
-            }
-        }
-        return message;
+       return UploadFile.uploadMultipleFiles(files);
     }
 }
