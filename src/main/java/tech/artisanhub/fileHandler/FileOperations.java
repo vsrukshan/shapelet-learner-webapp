e07a1f64de023c460add542370a05444754a6ec7
rnavagamuwa <randikanavagamuwa@gmail.com>
2017-01-10 18:17:38 +0530

    Important shapelets saving as JSON completed
    Important shapelets saving as JSON completed
    
    Important shapelets saving as JSON completed
    
    Important shapelets saving as JSON completed
    
    Important shapelets saving as JSON completed

--- src/main/java/tech/artisanhub/fileHandler/FileOperations.java
+++ src/main/java/tech/artisanhub/fileHandler/FileOperations.java
@@ -4,33 +4,56 @@ package tech.artisanhub.fileHandler;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FileOperations {
-    public static JSONObject filesInDir(String path)
-    {
+    public static JSONObject filesInDir(String path) {
         File file = new File(path);
         File[] files = file.listFiles();
         JSONObject jsonObject;
         JSONArray jsonArray = new JSONArray();
 
-        for(File f: files){
+        for (File f : files) {
             jsonObject = new JSONObject();
-            jsonObject.put("Last Modified",convertTime(f.lastModified()));
-            jsonObject.put("File Name",f.getName());
+            jsonObject.put("Last Modified", convertTime(f.lastModified()));
+            jsonObject.put("File Name", f.getName());
             jsonArray.add(jsonObject);
         }
         JSONObject mainObj = new JSONObject();
-        mainObj.put("Files on Server",jsonArray);
+        mainObj.put("Files on Server", jsonArray);
         return mainObj;
     }
 
-    public static String convertTime(long time){
+    public static String convertTime(long time) {
         Date date = new Date(time);
         Format format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return format.format(date);
+    }
+
+    public static boolean saveImportantShapelets(JSONObject finalJsonObject, String datasetName) {
+        String rootPath = System.getProperty("catalina.home");
+        BufferedWriter output = null;
+
+        File dir = new File(rootPath + File.separator + "generatedShapelets");
+        if (!dir.exists())
+            dir.mkdirs();
+
+        File file = new File(dir.getAbsolutePath() + File.separator + datasetName + ".json");
+
+        try {
+            output = new BufferedWriter(new FileWriter(file));
+            output.write(finalJsonObject.toJSONString());
+            output.close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+
     }
 }
